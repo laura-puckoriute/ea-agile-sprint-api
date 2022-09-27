cc28fbfae19f167ac1da472f324d5a1ab54afac6
Andrei Blanaru <florian-andrei.blanaru@kainos.com>
2022-09-27 14:47:15 +0100

    Refactored job table with description, capabilities and band level fields.

--- src/main/mysql/employeesdb.sql
+++ src/main/mysql/employeesdb.sql
@@ -6,6 +6,9 @@ CREATE DATABASE ea_agile_sprint_FlorianB;
 USE ea_agile_sprint_FlorianB;
 
 CREATE TABLE `Role` (
-	`title` varchar(50)
+	`title` varchar(50),
+    `description` varchar(2048),
+    `capability` ENUM('Engineering', 'Platforms', 'Data', 'Artificial Intelligence', 'Cyber Security', 'Workday', 'Experience Design', 'Product', 'Delivery', 'Operations', 'Business Development and Marketing', 'Organisational Strategy and Planning', 'People', 'Commercial and Financial Management', 'Business Services Support'),
+    `band_level` ENUM('Principal', 'Manager', 'Consultant', 'Senior Associate', 'Associate', 'Trainee', 'Apprentice')
 );
 
