c0c03a8178c42ee97397c6bfc4d3dafc17ba7ceb
Andrei Blanaru <florian-andrei.blanaru@kainos.com>
2022-09-27 14:09:24 +0100

    Added drop database to the database script.

--- src/main/mysql/employeesdb.sql
+++ src/main/mysql/employeesdb.sql
@@ -1,6 +1,11 @@
+DROP DATABASE IF EXISTS ea_agile_sprint_FlorianB;
+
+#Change username to yours.
 CREATE DATABASE ea_agile_sprint_FlorianB;
 
+USE ea_agile_sprint_FlorianB;
+
 CREATE TABLE `Role` (
-	`title` varchar(50),
+	`title` varchar(50)
 );
 
