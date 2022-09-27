a11c57366133bda2bd6e4eaad324725e7350c975
Laura Puckoriute <laura.puckoriute@kainos.com>
2022-09-27 09:53:04 +0100

    API skeleton setup

---
+++ src/main/java/org/kainos/ea/APIApplication.java
@@ -0,0 +1,30 @@
+package org.kainos.ea;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.kainos.ea.resources.WebService;
+
+public class APIApplication extends Application<APIConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new APIApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "API";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<APIConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final APIConfiguration configuration,
+                    final Environment environment) {
+        environment.jersey().register(new WebService());
+    }
+
+}
---
+++ src/main/java/org/kainos/ea/resources/WebService.java
@@ -0,0 +1,7 @@
+package org.kainos.ea.resources;
+
+import javax.ws.rs.Path;
+
+@Path("/api")
+public class WebService {
+}
