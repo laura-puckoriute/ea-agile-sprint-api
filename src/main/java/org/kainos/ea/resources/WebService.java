ac0a5b13967a313881167450491d374efda51c3d
mei chen <mei.chen@kainos.com>
2022-09-28 15:22:19 +0100

    GET /job-specification/{id} endpoint added

--- src/main/java/org/kainos/ea/resources/WebService.java
+++ src/main/java/org/kainos/ea/resources/WebService.java
@@ -7,11 +7,14 @@ import org.kainos.ea.models.JobRoles;
 import org.kainos.ea.service.JobsService;
 import org.kainos.ea.util.DatabaseConnection;
 
+import javax.print.attribute.standard.Media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.crypto.Data;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -33,6 +36,22 @@ public class WebService {
         } catch (SQLException | DatabaseConnectionException e) {
             System.out.println(e);
             return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+        }
+    }
+
+    @GET
+    @Path("/job-specification/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJobSpecification( @PathParam("id") int id ) throws SQLException, DatabaseConnectionException {
+
+        try {
+
+            return Response.ok(jobsService.getJobSpecification( id )).build();
+
+        } catch ( SQLException | DatabaseConnectionException e ) {
+
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
         }
     }
 }
