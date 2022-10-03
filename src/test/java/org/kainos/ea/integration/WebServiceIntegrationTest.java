97e87bd177eb90f3ebbb17014dde14d76cc1b125
Matthew Knox <matthew.knox@kainos.com>
2022-10-03 10:12:40 +0100

    adding integration test

--- src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package org.kainos.ea.integration;
 
 import org.kainos.ea.APIApplication;
 import org.kainos.ea.APIConfiguration;
+import org.kainos.ea.models.Competency;
 import org.kainos.ea.models.JobRolesResponse;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
@@ -23,6 +24,15 @@ public class WebServiceIntegrationTest {
     @Test
     void getJobRoles_shouldReturnListOfJobRoles() {
         List<JobRolesResponse> response = APP.client().target("http://localhost:8080/api/job-roles")
+                .request()
+                .get(List.class);
+
+        Assertions.assertTrue(response.size() > 0);
+    }
+
+    @Test
+    void getCompetencyByBandLevel_shouldReturnListOfCompetencies() {
+        List<Competency> response = APP.client().target("http://localhost:8080/api/competencies/6")
                 .request()
                 .get(List.class);
 
