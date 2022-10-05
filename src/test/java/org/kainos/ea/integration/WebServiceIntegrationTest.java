499c3f6c48b94011aad4cef4e0d15e49e4a89e3a
mei chen <mei.chen@kainos.com>
2022-10-05 09:18:45 +0100

    added test for band level id not found

--- src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
@@ -62,6 +62,16 @@ public class WebServiceIntegrationTest {
     }
 
     @Test
+    void getCompetencyByBandLevel_shouldReturn404_whenDataNotFoundExceptionThrown() {
+
+        Response response = APP.client().target( hostURI + "/competencies/99999999999" )
+                .request()
+                .get(Response.class);
+
+        Assertions.assertEquals( response.getStatus(), HttpStatus.NOT_FOUND_404 );
+    }
+
+    @Test
     void getJobSpecification_shouldReturnJobSpecification_whenJobServiceReturnsJobSpecification() {
         JobSpecificationResponse expectedResult = new JobSpecificationResponse("Software Engineer", "As a Trainee Software Engineer with Kainos, you will work on projects where you can make a real difference to people’s lives – the lives of people you know. After taking part in our award-winning, seven-week Engineering Academy, you will then join one of our many project teams, to learn from our experienced developers, project managers and customer-facing staff. You’ll have great support and mentoring, balanced with the experience of being given real, meaningful work to do, to help you truly develop both technically and professionally.",
             "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Software%20Engineer%20%28Trainee%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1");
