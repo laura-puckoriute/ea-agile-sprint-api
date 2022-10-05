a3df432b6cd4cc3cfe18d4c81fe99174d051af36
mei chen <mei.chen@kainos.com>
2022-10-05 09:14:26 +0100

    changed query

--- src/main/java/org/kainos/ea/data/CompetencyData.java
+++ src/main/java/org/kainos/ea/data/CompetencyData.java
@@ -3,28 +3,34 @@ package org.kainos.ea.data;
 import org.kainos.ea.exception.DatabaseConnectionException;
 import org.kainos.ea.models.Competency;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CompetencyData {
 
     public List<Competency> getCompetenciesByBandLevel(int id, Connection c) throws SQLException, DatabaseConnectionException {
-        Statement st = c.createStatement();
 
-        ResultSet rs = st.executeQuery(
-                "SELECT Competency.id as id, Competency.title as title, Responsibility.responsibility_name as responsibility_name, Responsibility.description as responsibility_description " +
-                        "FROM Competency " +
-                        "JOIN Responsibility ON Competency.id = Responsibility.CompetencyID " +
-                        "JOIN Band_Level ON Responsibility.band_levelID = Band_Level.id " +
-                        "WHERE Band_Level.id = " + id + ";");
+        String query =
+                "SELECT Competency.id as id, " +
+                "Competency.title as title, " +
+                "Responsibility.responsibility_name as responsibility_name, " +
+                "Responsibility.description as responsibility_description " +
+                "FROM Competency " +
+                "JOIN Responsibility ON Competency.id = Responsibility.CompetencyID " +
+                "JOIN Band_Level ON Responsibility.band_levelID = Band_Level.id " +
+                "WHERE Band_Level.id = ?;";
+
+        PreparedStatement st = c.prepareStatement( query );
+
+        st.setInt( 1, id );
+
+        ResultSet rs = st.executeQuery();
 
         List<Competency> competencies = new ArrayList<>();
 
-        while (rs.next()) {
+        while ( rs.next() ) {
+
             Competency competency = new Competency(
                     rs.getInt("id"),
                     rs.getString("title"),
@@ -34,6 +40,7 @@ public class CompetencyData {
 
             competencies.add(competency);
         }
+
         return competencies;
     }
 }
