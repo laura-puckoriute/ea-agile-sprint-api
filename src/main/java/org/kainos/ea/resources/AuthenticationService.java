4a027c421cc0dbdf12570b157bbea6a0a4a08c66
mei chen <mei.chen@kainos.com>
2022-10-06 16:23:22 +0100

    /signout endpoint takes in token

--- src/main/java/org/kainos/ea/resources/AuthenticationService.java
+++ src/main/java/org/kainos/ea/resources/AuthenticationService.java
@@ -1,5 +1,6 @@
 package org.kainos.ea.resources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.eclipse.jetty.http.HttpStatus;
 import org.kainos.ea.data.UserData;
 import org.kainos.ea.exception.DatabaseConnectionException;
@@ -7,6 +8,7 @@ import org.kainos.ea.exception.InvalidUserCredentialsException;
 import org.kainos.ea.models.User;
 import org.kainos.ea.service.UserService;
 import org.kainos.ea.util.DatabaseConnection;
+import org.kainos.ea.util.JwtToken;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -24,6 +26,35 @@ public class AuthenticationService {
     }
 
 
+    @GET
+    @Path("/gen")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response generateToken( String email ) {
+
+        try {
+            return Response.ok(JwtToken.generateToken( email )).build();
+        } catch ( Exception e ) {
+            System.out.println( e );
+            return Response.status( HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+        }
+    }
+
+    @POST
+    @Path("/verify")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response verifyToken( String token ) {
+
+        try {
+
+            return Response.ok( JwtToken.verifyToken( token ) ).build();
+
+        } catch ( Exception e ) {
+
+            System.out.println( e );
+            return Response.status( HttpStatus.INTERNAL_SERVER_ERROR_500).build();
+        }
+    }
     @POST
     @Path("/signin")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -41,6 +72,23 @@ public class AuthenticationService {
         } catch ( InvalidUserCredentialsException e ) {
 
             return Response.status( HttpStatus.UNAUTHORIZED_401 ).entity( e.getMessage() ).build();
+        }
+    }
+
+    @POST
+    @Path("/signout")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response removeUserToken( String token ) {
+
+        try {
+
+            return Response.ok( userService.removeUserToken( token ) ).build();
+
+        } catch ( SQLException | DatabaseConnectionException e ) {
+
+            return Response.status( HttpStatus.INTERNAL_SERVER_ERROR_500 ).build();
+
         }
     }
 }
