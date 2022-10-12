491c8bc32e23087b9338a149c91879a1ab890bf8
mei chen <mei.chen@kainos.com>
2022-10-12 12:09:41 +0100

    integration tests for getJobRole, updateJobRole, getBandLevels, getCapabilities

--- src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
+++ src/test/java/org/kainos/ea/integration/WebServiceIntegrationTest.java
@@ -15,16 +15,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class WebServiceIntegrationTest {
+
     String hostURI = System.getenv("API_URL");
+
     static final DropwizardAppExtension<APIConfiguration> APP = new DropwizardAppExtension<>(
             APIApplication.class, null,
             new ResourceConfigurationSourceProvider()
@@ -32,15 +32,16 @@ public class WebServiceIntegrationTest {
 
 
     @Test
-    void getJobRoles_shouldReturnListOfJobRoles_withIdTitleCapability() throws UnsupportedEncodingException {
+    void getJobRoles_shouldReturnListOfJobRoles_withIdTitleCapability() {
+
         JsonNode response = APP.client().target( hostURI + "/job-roles" )
                 .request()
-                .get(JsonNode.class);
+                .get( JsonNode.class );
 
         Assertions.assertTrue(response.size() > 0);
 
-
         ObjectMapper mapper = new ObjectMapper();
+
         List<JobRolesResponse> jobList = mapper.convertValue(response, new TypeReference<List<JobRolesResponse>>(){});
 
         Assertions.assertEquals("Software Engineer", jobList.get(0).getTitle());
@@ -58,7 +59,7 @@ public class WebServiceIntegrationTest {
 
         CompetenciesWithBandLevel response = APP.client().target( hostURI + "/competencies/6" )
                 .request()
-                .get(CompetenciesWithBandLevel.class);
+                .get( CompetenciesWithBandLevel.class );
 
         Assertions.assertTrue( response.getBandLevel().equals( bandLevel ) );
         Assertions.assertTrue( response.getCompetencies().size() > 0 );
@@ -69,35 +70,118 @@ public class WebServiceIntegrationTest {
 
         Response response = APP.client().target( hostURI + "/competencies/99999999999" )
                 .request()
-                .get(Response.class);
+                .get( Response.class );
 
         Assertions.assertEquals( response.getStatus(), HttpStatus.NOT_FOUND_404 );
     }
     
     @Test
-    void getJobSpecification_shouldReturnJobSpecification_whenJobServiceReturnsJobSpecification() throws UnsupportedEncodingException {
+    void getJobRole_shouldReturnJobRolesResponse_whenJobServiceReturnsJobRole() {
 
-        JobSpecificationResponse expectedResult = new JobSpecificationResponse(
-                "Software Engineer",
-                "•Completed or are currently studying a relevant third level IT qualification\n•Familiar with some programming languages and implementation environments.\n•Some understanding of the software development lifecycle from your studies or relevant work experience, and the relevance of different tools at different stages of the development lifecycle\n•Able to make effective decisions with the support of team members, within fast-moving delivery environment.\n•Have an open attitude to sharing knowledge and information.\n•Ideally have some experience of working in a collaborative team environment\n•Good communication skills with the ability to communicate issues to other technical people and, sometimes, non-technical people\n•Good problem solving and analytical skills.\n•We all work in teams here in Kainos – a proven ability of strong team skills, including taking direction from others, is crucial.\n•Ability to carry out responsibilities in accordance with company policies, procedures and processes.•Ability to deliver tasks within a given timeframe. ",
-                "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Software%20Engineer%20%28Trainee%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1",
-                "As a Trainee Software Engineer with Kainos, you will work on projects where you can make a real difference to people’s lives – the lives of people you know. After taking part in our award-winning, seven-week Engineering Academy, you will then join one of our many project teams, to learn from our experienced developers, project managers and customer-facing staff. You’ll have great support and mentoring, balanced with the experience of being given real, meaningful work to do, to help you truly develop both technically and professionally.");
+        JobRoleResponse expectedResult = new JobRoleResponse(
+                2,
+                "Technical Architect",
+                "•Experience delivering software designs for multi-tiered modern software applications.\n•Experience of technical ownership for a product/software project, including architecture, estimation, product planning and user story/requirement creation.\n•Understands non-functional concerns for customers and has experience incorporating these into the application design.\n•Has experience with public cloud platforms, such as AWS and Azure, including SaaS and PaaS offerings.\n•Able to simply and clearly communicate technical design in conversation, documentation and presentations.\n•Able to make effective decisions within fast-moving delivery.\n•We are passionate about developing people – a demonstrated ability in managing, coaching and developing junior members of your team and wider community. ",
+                "As a Technical Architect (Consultant) in Kainos, you’ll be responsible for leading teams and developing high quality solutions which delight our customers and impact the lives of users worldwide. As a technical leader on a project, you’ll work with customer architects to agree technical designs, advising on estimated effort and technical implications of user stories and user journeys. You’ll manage, coach and develop a small number of staff, with a focus on managing employee performance and assisting in their career development. It’s a fast-paced environment so it is important for you to make sound, reasoned decisions. You’ll do this whilst learning about new technologies and approaches, with room to learn, develop and grow.",
+                "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Technical%20Architect%20%28Consultant%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1",
+                "Consultant",
+                3,
+                "Engineering",
+                1,
+                "Engineering");
 
-        JobSpecificationResponse response = APP.client().target( hostURI + "/job-specification/1")
+        JobRoleResponse response = APP.client().target( hostURI + "/job-roles/2")
                 .request()
-                .get(JobSpecificationResponse.class);
+                .get( JobRoleResponse.class );
 
-        Assertions.assertEquals(expectedResult, response);
+        Assertions.assertTrue( expectedResult.equals( response ) );
     }
 
     @Test
-    void getJobSpecification_shouldReturnErrorStatus404() throws UnsupportedEncodingException {
+    void getJobRole_shouldReturn404_whenDataNotFoundExceptionThrown() {
 
-        Response expectedResponse = Response.status(HttpStatus.NOT_FOUND_404).build();
-        Response response = APP.client().target(hostURI + "/job-specification/99999999999")
+        Response expectedResponse = Response.status( HttpStatus.NOT_FOUND_404 ).build();
+
+        Response response = APP.client().target(hostURI + "/job-roles/99999999999")
                 .request()
-                .get(Response.class);
+                .get( Response.class );
 
-        Assertions.assertEquals(expectedResponse.getStatus(), response.getStatus());
+        Assertions.assertEquals( expectedResponse.getStatus(), response.getStatus() );
+    }
+
+    @Test
+    void updateJobRole_shouldReturn200_whenSuccessful() {
+
+        JobRoleRequest request = new JobRoleRequest(
+                "Technical Architect",
+                "•Experience delivering software designs for multi-tiered modern software applications.\n•Experience of technical ownership for a product/software project, including architecture, estimation, product planning and user story/requirement creation.\n•Understands non-functional concerns for customers and has experience incorporating these into the application design.\n•Has experience with public cloud platforms, such as AWS and Azure, including SaaS and PaaS offerings.\n•Able to simply and clearly communicate technical design in conversation, documentation and presentations.\n•Able to make effective decisions within fast-moving delivery.\n•We are passionate about developing people – a demonstrated ability in managing, coaching and developing junior members of your team and wider community. ",
+                "As a Technical Architect (Consultant) in Kainos, you’ll be responsible for leading teams and developing high quality solutions which delight our customers and impact the lives of users worldwide. As a technical leader on a project, you’ll work with customer architects to agree technical designs, advising on estimated effort and technical implications of user stories and user journeys. You’ll manage, coach and develop a small number of staff, with a focus on managing employee performance and assisting in their career development. It’s a fast-paced environment so it is important for you to make sound, reasoned decisions. You’ll do this whilst learning about new technologies and approaches, with room to learn, develop and grow.",
+                "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Technical%20Architect%20%28Consultant%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1",
+                3,
+                1);
+
+        int response = APP.client().target( hostURI + "/job-roles/2")
+                .request()
+                .put( Entity.entity( request, MediaType.APPLICATION_JSON ) )
+                .getStatus();
+
+        Assertions.assertEquals( HttpStatus.OK_200, response );
+    }
+
+    @Test
+    void updateJobRole_shouldReturn404_whenJobRoleNotFound() {
+
+        JobRoleRequest request = new JobRoleRequest(
+                "Technical Architect",
+                "•Experience delivering software designs for multi-tiered modern software applications.\n•Experience of technical ownership for a product/software project, including architecture, estimation, product planning and user story/requirement creation.\n•Understands non-functional concerns for customers and has experience incorporating these into the application design.\n•Has experience with public cloud platforms, such as AWS and Azure, including SaaS and PaaS offerings.\n•Able to simply and clearly communicate technical design in conversation, documentation and presentations.\n•Able to make effective decisions within fast-moving delivery.\n•We are passionate about developing people – a demonstrated ability in managing, coaching and developing junior members of your team and wider community. ",
+                "As a Technical Architect (Consultant) in Kainos, you’ll be responsible for leading teams and developing high quality solutions which delight our customers and impact the lives of users worldwide. As a technical leader on a project, you’ll work with customer architects to agree technical designs, advising on estimated effort and technical implications of user stories and user journeys. You’ll manage, coach and develop a small number of staff, with a focus on managing employee performance and assisting in their career development. It’s a fast-paced environment so it is important for you to make sound, reasoned decisions. You’ll do this whilst learning about new technologies and approaches, with room to learn, develop and grow.",
+                "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Technical%20Architect%20%28Consultant%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&ga=1",
+                3,
+                1);
+
+        int response = APP.client().target( hostURI + "/job-roles/9999999999999")
+                .request()
+                .put( Entity.entity( request, MediaType.APPLICATION_JSON ) )
+                .getStatus();
+
+        Assertions.assertEquals( HttpStatus.NOT_FOUND_404, response );
+    }
+
+    @Test
+    void getBandLevels_shouldReturnBandLevelList() {
+
+        List<BandLevel> response = APP.client().target( hostURI + "/band-levels" )
+                .request()
+                .get( List.class );
+
+        Assertions.assertTrue( response.size() > 0 );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<BandLevel> bandLevels = mapper.convertValue( response, new TypeReference<List<BandLevel>>(){});
+
+        Assertions.assertEquals( 1, bandLevels.get(0).getId() );
+        Assertions.assertEquals("Principal", bandLevels.get(0).getBandName());
+        Assertions.assertEquals( 7, bandLevels.get(6).getId() );
+        Assertions.assertEquals("Apprentice", bandLevels.get(6).getBandName());
+    }
+
+    @Test
+    void getCapabilities_shouldReturnCapabilitiesList() {
+
+        List<Capability> response = APP.client().target( hostURI + "/capabilities" )
+                .request()
+                .get( List.class );
+
+        Assertions.assertTrue( response.size() > 0 );
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Capability> capabilities = mapper.convertValue( response, new TypeReference<List<Capability>>(){});
+
+        Assertions.assertEquals( 1, capabilities.get(0).getId() );
+        Assertions.assertEquals("Engineering", capabilities.get(0).getTitle());
+        Assertions.assertEquals( 15, capabilities.get(14).getId() );
+        Assertions.assertEquals("Business Services Support", capabilities.get(14).getTitle());
     }
 }
