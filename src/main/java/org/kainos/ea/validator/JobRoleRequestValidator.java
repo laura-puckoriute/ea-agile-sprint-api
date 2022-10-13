8a44d45858d4ffa4b524fdb00237686697cddd9f
mei chen <mei.chen@kainos.com>
2022-10-13 10:44:20 +0100

    added validator for jobrolerequest

---
+++ src/main/java/org/kainos/ea/validator/JobRoleRequestValidator.java
@@ -0,0 +1,39 @@
+package org.kainos.ea.validator;
+
+import org.kainos.ea.exception.*;
+import org.kainos.ea.models.JobRoleRequest;
+
+public class JobRoleRequestValidator {
+
+    public boolean isValidJobRole( JobRoleRequest jobRoleRequest ) throws JobRoleTitleEmptyException, BandLevelInvalidException, CapabilityInvalidException, JobFamilyInvalidException, JobRoleLinkInvalidException {
+
+        String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+        if ( jobRoleRequest.getTitle().length() < 0 ) {
+
+            throw new JobRoleTitleEmptyException();
+        }
+
+        if ( jobRoleRequest.getBandLevelID() < 0 ) {
+
+            throw new BandLevelInvalidException();
+        }
+
+        if ( jobRoleRequest.getCapabilityID() < 0 ) {
+
+            throw new CapabilityInvalidException();
+        }
+
+        if ( jobRoleRequest.getJobFamilyID() < 0 ) {
+
+            throw new JobFamilyInvalidException();
+        }
+
+        if ( jobRoleRequest.getLink().length() > 0 ) {
+
+            if ( !jobRoleRequest.getLink().matches( urlRegex ) ) throw new JobRoleLinkInvalidException();
+        }
+
+        return true;
+    }
+}
